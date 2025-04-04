ef98346d5baef28fb1db916a824934866fe127c8
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2025-04-04 10:24:44 +0000

    quarto commit

--- lista della spesa/app.py
+++ lista della spesa/app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template #importiamo la classe flask
+from flask import Flask, render_template, request, redirect, url_for #importiamo la classe flask
 app = Flask(__name__) #inizializza app flask
-
-x=100
 lista=["ciao", "ciao1", "ciao2"]
+
 @app.route('/') #visitiamo (`/`), la funzione home() viene eseguita.
 def home():
     return render_template("index.html", lista=lista)
 
-
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    #ottiene elemento dal form
+    elemento = request.form['elemento']
+    
+    if elemento:
+        lista.append(elemento)
+    return redirect(url_for('home'))
 
 
 #avvio flask
