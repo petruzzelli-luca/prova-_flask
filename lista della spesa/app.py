aba6ae03395b7a1d4d1bf48c4b933d506c417595
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2025-04-04 08:56:24 +0000

    terzo commit

--- lista della spesa/app.py
+++ lista della spesa/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template #importiamo la classe flask
 app = Flask(__name__) #inizializza app flask
 
+x=100
+lista=["ciao", "ciao1", "ciao2"]
 @app.route('/') #visitiamo (`/`), la funzione home() viene eseguita.
 def home():
-    return render_template("index.html")
+    return render_template("index.html", lista=lista)
+
+
+
 
 #avvio flask
 if __name__ == '__main__':
